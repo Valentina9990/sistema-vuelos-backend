da2db0afbdc89e169eb05b84ea6574795b730907
Valentina9990 <dania.v.sarmiento@gmail.com>
2024-09-05 21:50:02 -0500

    Final data model

---
+++ src/test/java/com/example/caparepositorio/CapaRepositorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caparepositorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapaRepositorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
