50e7ea210ce3b3c8235397c44f63ad0ec4348224
Valentina9990 <dania.v.sarmiento@gmail.com>
2024-09-05 22:08:53 -0500

    Add repositories

--- src/main/java/com/example/caparepositorio/repositories/VueloRepository.java
+++ src/main/java/com/example/caparepositorio/repositories/VueloRepository.java
@@ -1,6 +1,6 @@
 package com.example.caparepositorio.repositories;
-import com.example.caparepositorio.entities.Cliente;
+import com.example.caparepositorio.entities.Vuelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository {
+public interface VueloRepository {
 }
